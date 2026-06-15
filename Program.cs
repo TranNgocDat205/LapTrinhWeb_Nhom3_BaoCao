e0a7fbec7b3f95d50aeaee8e50984984e9173e50
TranNgocDat205 <tndat200905it@gmail.com>
2026-06-15 17:00:17 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IEmailService, SmtpEmailService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+   
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
